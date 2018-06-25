6dd68d8db31693d6cfc234cf537b12cd41ce9dd8
Mark Loreto <markyctrigger27@gmail.com>
2018-06-25 11:15:02 +0800

    *Improvements for add product screen (Now using form module)

--- src/pages/add-product/add-product.ts
+++ src/pages/add-product/add-product.ts
@@ -1,6 +1,8 @@
 import { MyFunctionProvider } from './../../providers/my-function/my-function';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import moment from 'moment'
 /**
  * Generated class for the AddProductPage page.
  *
@@ -15,27 +17,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddProductPage {
   segment: string = "details"
-  name: string = ""
-  cost: number
-  price: number
-  sku: string = ""
   category: any
-  measurement_unit: any
   categories: any = []
   measurement_units: any = []
   photo: any = ""
-  thumbnail: any
+  thumbnail: any = ""
   image: any
   shared: boolean = false
+  catName: string = ""
+  myForm: FormGroup;
+  submitAttempt: boolean = false;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public myFunctionProvider: MyFunctionProvider
+    public myFunctionProvider: MyFunctionProvider,
+    public formBuilder: FormBuilder
   ) {
+
+    this.myForm = formBuilder.group({
+      pName: ['', Validators.required],
+      measurement_unit: ['', Validators.required],
+      cost: ['', Validators.required],
+      price: ['', Validators.required],
+      pack: ['', Validators.required],
+      sku: [''],
+      category: [''],
+    });
     this.loadPhoto()
     this.category = (navParams.get("id")) ? navParams.get("id") : null
+
+    this.catSelected()
     this.myFunctionProvider.dbQuery("SELECT * FROM product_categories ORDER BY sequence", []).then((categories: any) => {
       this.categories = categories
+      this.myForm.controls.category.setValue(navParams.get("id"))
     })
 
     this.myFunctionProvider.dbQuery("SELECT * FROM measurement_units", []).then((mu: any) => {
@@ -49,6 +63,7 @@ export class AddProductPage {
 
   takePhoto(){
     this.myFunctionProvider.takePicture().then((photoData: any) => {
+      console.log("Photo Data", photoData)
       this.photo = photoData.photo
       this.thumbnail = photoData.thumbnail
       this.loadPhoto()
@@ -59,10 +74,48 @@ export class AddProductPage {
     this.image = (!this.photo) ? "assets/images/bc5.jpg" : this.myFunctionProvider.sanitize(this.photo)
   }
 
+  catSelected(){
+    this.myFunctionProvider.dbQuery("SELECT name FROM product_categories WHERE id = ?", [this.category]).then((cat: any) => {
+      if(cat.length)
+        this.catName = cat[0].name
+    })
+  }
+
   submit(){
-    if(this.sku == ""){
-      //this.myFunctionProvider.dbQuery("SELECT sku FROM products WHERE sku LIKE '%%'")
+    this.submitAttempt = true;
+    if(!this.myForm.valid)
+      return
+    let fc = this.myForm.controls
+    var genSKU = fc.sku.value
+    if(fc.sku.value == ""){
+      var l3 = this.catName.slice(0, 3).toUpperCase()
+      console.log(l3)
+      genSKU = l3 + moment().format("x")
+      console.log(genSKU)
     }
+
+    console.log(this.myForm)
+    this.myFunctionProvider.spinner(true, "Please wait")
+    this.myFunctionProvider.dbQuery("INSERT OR REPLACE INTO products (id, name, photo, thumbnail, category, cost, price, author, measurement_unit, shareable, sku, sequence, pack, sync) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [this.myFunctionProvider.getTimestamp(), fc.pName.value, this.photo, this.thumbnail, fc.category.value, fc.cost.value, fc.price.value, this.myFunctionProvider.getSettings("logged_staff"), fc.measurement_unit.value, (this.shared) ? 1 : 0, genSKU, 0, parseInt(fc.pack.value), 0]).then(() => {
+      this.myFunctionProvider.presentToast("New product saved", "my-success")
+      this.myFunctionProvider.spinner(false, "")
+      this.navCtrl.pop()
+    })
+
+  }
+
+  isValid(field){
+    return !this.myForm.controls[field].valid && (!!this.myForm.controls[field].dirty || this.submitAttempt)
+  }
+
+  uploadPhoto(){
+    this.myFunctionProvider.fileChooser.open().then(uri => {
+      this.myFunctionProvider.imageProcess(uri).then((photoData: any) => {
+        this.photo = photoData.photo
+        this.thumbnail = photoData.thumbnail
+        this.loadPhoto()
+      })
+    }).catch(e => console.log(e));
   }
 
 }
